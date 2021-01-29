02673c24968a81e9571d690ebb8dfe2cbdb0ef69
Shaunn <tbevan@gmail.com>
2021-01-28 22:13:13 -0500

    added black

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,22 +6,16 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ['id']
-    list_display = ['email', 'name']
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name',)}),
-        (
-            _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
-        ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("name",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
-        }),
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
     )
 
 
