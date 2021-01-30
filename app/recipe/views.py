cad477b60bcf547b565aa7a26694ce12e9e64afc
Shaunn <tbevan@gmail.com>
2021-01-30 13:19:21 -0500

    create ingredient api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,7 +27,9 @@ class TagViewSet(
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
     """Manage the ingredients in the database"""
 
     authentication_classes = (TokenAuthentication,)
@@ -38,3 +40,7 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
