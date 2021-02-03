eadc0863cba0afdae4fd5230ea25cd3a1a5c0dfd
Shaunn <tbevan@gmail.com>
2021-02-02 22:35:51 -0500

    Add recipe image to model

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/user/", include("user.urls")),
     path("v1/recipe/", include("recipe.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
